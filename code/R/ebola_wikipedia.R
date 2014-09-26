f0ad3754363a42caf7b5dde3b522c6a08a3ce358
Neil Saunders <neilfws@gmail.com>
2014-09-26 14:05:43 +1000

    Edited code for changes to Wikipedia page (2 tables) and new file locations

--- code/R/ebola_wikipedia.R
+++ code/R/ebola_wikipedia.R
@@ -1,24 +1,18 @@
 library(XML)
 library(ggplot2)
 library(reshape2)
- 
+
+setwd("~/Dropbox/projects/ebola")
 # get all tables on the page
 ebola <- readHTMLTable("http://en.wikipedia.org/wiki/Ebola_virus_epidemic_in_West_Africa", 
                   stringsAsFactors = FALSE)
 # thankfully our table has a name; it is table #5
-# this is not something you can really automate
-head(names(ebola))
-# [1] "Ebola virus epidemic in West Africa"          
-# [2] "Nigeria Ebola areas-2014"                     
-# [3] "Treatment facilities in West Africa"          
-# [4] "Democratic Republic of Congo-2014"            
-# [5] "Ebola cases and deaths by country and by date"
-# [6] "NULL"
- 
-ebola <- ebola$`Ebola cases and deaths by country and by date`
- 
+# NOT AS OF 2014-09-26! There are not 2 tables and they are 7, 8.
+ebola.current <- ebola[[7]]
+ebola.archive <- ebola[[8]]
+
 # again, manual examination reveals that we want rows 2-end and columns 1-3
-ebola.new <- ebola[2:nrow(ebola), 1:3]
+ebola.new <- rbind(ebola.current[2:nrow(ebola.current), 1:3], ebola.archive[2:nrow(ebola.archive), 1:3])
 colnames(ebola.new) <- c("date", "cases", "deaths")
  
 # need to fix up a couple of cases that contain text other than the numbers
@@ -34,7 +28,7 @@ ebola.new$deaths <- as.numeric(ebola.new$deaths)
 # the days in the dates are encoded 1-31
 # are we there yet? quick and dirty attempt to reproduce Wikipedia plot
 ebola.m <- melt(ebola.new)
-png("ebola.png", 800, 600)
+png("output/ebola.png", 800, 600)
 print(ggplot(ebola.m, aes(as.Date(date, "%e %b %Y"), value)) + 
        geom_point(aes(color = variable)) + 
        coord_trans(y = "log10") + xlab("Date") + 
