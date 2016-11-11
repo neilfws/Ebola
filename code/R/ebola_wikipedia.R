cdf78856b98e8987dd5048ce0daafcf364206e66
Neil Saunders <neilfws@gmail.com>
2016-11-11 16:30:18 +1100

    Rewrite for updated Wikipedia page + rvest, dplyr

--- code/R/ebola_wikipedia.R
+++ code/R/ebola_wikipedia.R
@@ -1,38 +1,27 @@
-library(XML)
+library(rvest)
 library(ggplot2)
-library(reshape2)
+library(dplyr)
 
 setwd("~/Dropbox/projects/github_projects/ebola")
-# get all tables on the page
-ebola <- readHTMLTable("http://en.wikipedia.org/wiki/Ebola_virus_epidemic_in_West_Africa", 
-                  stringsAsFactors = FALSE)
-# thankfully our table has a name; it is table #5
-# NOT AS OF 2014-09-26! There are now 2 tables and they are 7, 8.
-# NOT AS OF 2014-10-02! There are now 2 tables and they are 8, 9.
-ebola.current <- ebola[[5]]
-ebola.archive <- ebola[[7]]
+# get the table of cases
+u <- "https://en.wikipedia.org/wiki/West_African_Ebola_virus_epidemic_timeline_of_reported_cases_and_deaths"
+cases <- read_html(u) %>% html_nodes("table") %>% .[[2]] %>% html_table(fill = TRUE)
 
-# again, manual examination reveals that we want rows 2-end and columns 1-3
-ebola.new <- rbind(ebola.current[2:nrow(ebola.current), 1:3], ebola.archive[2:nrow(ebola.archive), 1:3])
-colnames(ebola.new) <- c("date", "cases", "deaths")
- 
-# need to fix up a couple of cases that contain text other than the numbers
-#ebola.new$cases[nrow(ebola.new)-43]  <- "759"
-#ebola.new$deaths[nrow(ebola.new)-43] <- "467"
- 
 # get rid of the commas; convert to numeric
-ebola.new$cases  <- gsub(",", "", ebola.new$cases)
-ebola.new$cases  <- as.numeric(ebola.new$cases)
-ebola.new$deaths <- gsub(",", "", ebola.new$deaths)
-ebola.new$deaths <- as.numeric(ebola.new$deaths)
- 
-# the days in the dates are encoded 1-31
-# are we there yet? quick and dirty attempt to reproduce Wikipedia plot
-ebola.m <- melt(ebola.new)
+cases[, 2:9] <- apply(cases[, 2:9], 2, function(x) gsub(",", "", x))
+cases[, 2:9] <- apply(cases[, 2:9], 2, as.numeric)
+
+colnames(cases) <- c("date", "cases.total", "deaths.total", "cases.guinea", "deaths.guinea", "cases.liberia", "deaths.liberia", "cases.sierra_leone", "deaths.sierra_leone", "sources")
+cases$Date <- as.Date(cases$date, "%d %b %Y")
+
+# gather the cases/deaths into 1 column
+cases.1 <- gather(cases, key, value, -Date, -date, -sources)
+
+# then split into country
+cases.2 <- separate(cases.1, key, into = c("count", "country"), sep = "\\.")
+
+
+# are we there yet? quick and dirty attempt to reproduce Wikipedia plots
 png("output/ebola.png", 800, 600)
-print(ggplot(ebola.m, aes(as.Date(date, "%e %b %Y"), value)) + 
-       geom_point(aes(color = variable)) + 
-       coord_trans(y = "log10") + xlab("Date") + 
-       labs(title = "Cumulative totals log scale") + 
-       theme_bw())
+print(ggplot(cases.2, aes(Date, value)) + geom_point(aes(color = count)) + facet_wrap(~ country) + theme_bw() + scale_color_manual(values = c("skyblue3", "darkorange")) + labs(y = "count", title = "Cumulative total Ebola cases"))
 dev.off()
